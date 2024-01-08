900403e27f0842b2af8b23a4ca959556b5f8f38a
Yatin Chaudhari <c.yatin727@gmail.com>
2024-01-08 15:20:36 +0530

    Update Page.js

--- src/components/Page.js
+++ src/components/Page.js
@@ -3,15 +3,18 @@ import { LocalThemedBox } from './LocalThemedBox';
 import { ThemeContext } from './ThemeProvider';
 
 const Page = () => {
+  const { theme } = React.useContext(ThemeContext);
 
     return(
-        <div className={"container"} id="themed-page">
-            <p id="themed-text-container">
-                lorem ipsum dolor iterit n stuff
-            </p>
-            <button className="btn" id="themed-button">Themed Button</button>
-            <LocalThemedBox />
-        </div>
+      <div className={`container ${theme === 'light' ? 'bg-light' : 'bg-dark'}`} id="themed-page">
+      <p id="themed-text-container" className={`txt-${theme}`}>
+        lorem ipsum dolor iterit n stuff
+      </p>
+      <button className={`btn ${theme === 'light' ? 'btn-light txt-light' : 'btn-dark txt-dark'}`} id="themed-button">
+        Themed Button
+      </button>
+      <LocalThemedBox />
+    </div>
     )
 }
 
